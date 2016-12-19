ee361c7fa9a35326f44bcdafe8a6a6dc0c69d43f
Ren Fukano <fuka-no-ren@outlook.jp>
2016-12-19 11:38:18 +0900

    レイアウト直したり...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :check_logined
   private
-  def check_logined
-    if session[:usr] then
-      begin
-        @usr = User.find(session[:usr])
-      rescue ActiveRecord::RecordNotFound
-        reset_session
+    def check_logined
+      if session[:usr] then
+        begin
+          @usr = User.find(session[:usr])
+        rescue ActiveRecord::RecordNotFound
+          reset_session
+        end
+      end
+      unless @usr
+        flash[:referer] = request.fullpath
+        redirect_to controller: :login, action: :index
       end
     end
-    unless @usr
-      flash[:referer] = request.fullpath
-      redirect_to controller: :login, action: :index
+    def log_out
+      session.delete(:user.id)
+      @session[:usr] = nil
+      @session[:usrname]
     end
-  end
-  def log_out
-    session.delete(:user.id)
-    @session[:usr] = nil
-    @session[:usrname]
-  end
 end
--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,8 +1,6 @@
 class DepartmentsController < ApplicationController
   before_action :set_department, only: [:show, :edit, :update, :destroy]
 
-
-
   # GET /departments
   # GET /departments.json
   def index
